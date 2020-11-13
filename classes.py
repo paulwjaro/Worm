d51776403dfd1f403cc7016879325b838c3f4417
Paul <paulwjaroslawski@gmail.com>
2020-11-13 12:24:08 -0500

    Finished Snake clone.

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 
@@ -6,7 +7,6 @@ START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 class Snake:
     def __init__(self):
         self.snake_pieces = []
-        self.directions = {}
         self.head = None
 
     def move_snake(self):
@@ -19,10 +19,8 @@ class Snake:
 
     def build_snake(self):
         for i in range(3):
-            snake = Turtle(shape="square")
-            snake.color("green")
-            snake.penup()
-            self.snake_pieces.append(snake)
+            self.add_segment()
+            snake = self.snake_pieces[i]
             snake.goto(START_POSITIONS[i])
         self.head = self.snake_pieces[0]
 
@@ -42,10 +40,54 @@ class Snake:
         if self.head.heading() != 0:
             self.head.setheading(180)
 
+    def grow_snake(self):
+        self.add_segment()
+        snake_size = len(self.snake_pieces)
+        snake = self.snake_pieces[snake_size - 1]
+        snake.setx(self.snake_pieces[snake_size - 2].xcor())
+        snake.sety(self.snake_pieces[snake_size - 2].ycor())
 
-class Food:
-    pass
+    def add_segment(self):
+        snake = Turtle(shape="square")
+        snake.color("green")
+        snake.penup()
+        self.snake_pieces.append(snake)
 
 
-class Score:
-    pass
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(0.75, 0.75)
+        self.penup()
+        self.color("red")
+        self.speed("fastest")
+        self.x = 0
+        self.y = 0
+        self.eaten()
+
+    def eaten(self):
+        self.x = random.randint(-280, 280)
+        self.y = random.randint(-280, 280)
+        self.goto(self.x, self.y)
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.speed("fastest")
+        self.sety(260)
+        self.score = 0
+
+    def display_score(self):
+        self.write(f"Score: {self.score}", move=False, align="center", font=("Arial", 12, "normal"))
+
+    def update_score(self):
+        self.score += 10
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER!!", move=False, align="center", font=("Arial", 24, "normal"))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
-from classes import Snake
+from classes import Snake, Food, Score
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +10,8 @@ screen.tracer(0)
 gameon = True
 screen.listen()
 player = Snake()
+food = Food()
+score = Score()
 player.build_snake()
 screen.onkey(player.up, "Up")
 screen.onkey(player.down, "Down")
@@ -21,12 +23,33 @@ def end_game():
     global gameon
     gameon = False
 
+
 screen.onkey(end_game, "q")
 
 while gameon:
+    score.clear()
+    score.display_score()
     screen.update()
     time.sleep(0.1)
     player.move_snake()
 
-screen.bye()
-screen.mainloop()
+    # collision check
+    if player.head.distance(food) <= 20:
+        food.eaten()
+        score.update_score()
+        player.grow_snake()
+
+    # collision with tail
+    for i in player.snake_pieces[1:]:
+        if player.head.distance(i) <= 10:
+            gameon = False
+
+    # detect wall
+    if player.head.xcor() > 290 or player.head.xcor() < -300 or player.head.ycor() > 290 or player.head.ycor() < -290:
+        gameon = False
+
+
+food.hideturtle()
+score.game_over()
+
+screen.exitonclick()
